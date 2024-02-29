feb4cb3c33e5546a31d2f3af926d65c475f22b15
Richard <147564855+jr0612@users.noreply.github.com>
2024-02-28 20:43:29 -0500

    Update operations.py

--- operations.py
+++ operations.py
@@ -1,5 +1,5 @@
-def basic_operation(first_number: int,  second_number: int, sing: str):
-    match sing:
+def basic_operation(first_number: int,  second_number: int, sign: str):
+    match sign:
         case  "+":
             return first_number + second_number
         case  "-":
